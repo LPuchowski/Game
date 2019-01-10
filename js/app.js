41efe5815b8ee2959a1748cbca98690bce305edd
Tech-a44 <tech-a44@Tech-A44s-MacBook-Air.local>
2019-01-10 09:36:00 +0000

    Two player match and tournament mode functionality complete

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(function(){
 
   var interval;
+  var interval2;
   var progRunning = false;
   var i = 1;
 
@@ -38,7 +39,11 @@ $(function(){
   var winner = null;
   var roundCount = 1;
   var roundBegin = false;
-  var filtered;
+  var filtered = [0];
+  var tournament = false;
+  var playerArray;
+  var round1Winners;
+  var t1finished = false;
 
   Array.prototype.allValuesSame = function() {
 
@@ -51,306 +56,6 @@ $(function(){
     return true;
   }
 
-  $("#btn").click(function(){
-    while(paddle1Color != "1" && paddle1Color != "2" && paddle1Color != "3" && paddle1Color != "4" && paddle1Color != "5") {
-      var paddle1Color = prompt("Player 1, choose your paddle colour: 1) Red, 2) Yellow, 3) Blue, 4) Orange, 5) Purple");
-    }
-    while(paddle2Color != "1" && paddle2Color != "2" && paddle2Color != "3" &&    paddle2Color != "4" && paddle2Color != "5") {
-      var paddle2Color = prompt("Player 2, choose your paddle colour: 1) Red, 2) Yellow, 3) Blue, 4) Orange, 5) Purple");
-    }
-    switch (paddle1Color) {
-      case "1": paddle1Color = "Red"; break;
-      case "2": paddle1Color = "Yellow"; break;
-      case "3": paddle1Color = "Blue"; break;
-      case "4": paddle1Color = "Orange"; break;
-      case "5": paddle1Color = "Purple"; break;
-      default: break;
-    }
-    switch (paddle2Color) {
-      case "1": paddle2Color = "Red"; break;
-      case "2": paddle2Color = "Yellow"; break;
-      case "3": paddle2Color = "Blue"; break;
-      case "4": paddle2Color = "Orange"; break;
-      case "5": paddle2Color = "Purple"; break;
-      default: break;
-    }
-    paddle.css("backgroundColor", paddle1Color);
-    paddle2.css("backgroundColor", paddle2Color);
-    playGame("1", "2");
-  })
-
-  $("#tournament").click(function(){
-    var players = prompt("How many players? Please enter an even number");
-    while (!(players === 0 || !!(players && !(players%2)))) {
-      players = prompt("You need to enter an even number. How many players?");
-    }
-    console.log(players);
-    var playerArray = new Array(parseInt(players));
-    console.log(playerArray);
-    for (var i = 0; i < parseInt(players); i++) {
-      playerArray[i] = i + 1;
-    }
-    function random_item(items) {
-      return items[Math.floor(Math.random()*items.length)];
-    }
-    var player1 = random_item(playerArray);
-    do {
-      var player2 = random_item(playerArray);
-    } while (player1 == player2);
-
-    playerArray[player1-1] = null;
-    playerArray[player2-1] = null;
-
-    var round1Winners = new Array(playerArray.length/2);
-    var count = 0;
-
-    alert("First match is Player " + player1 + " vs Player " + player2);
-
-    playGame(player1, player2);
-  })
-
-  $("#tournament").click(function(){
-    var players = prompt("How many players? Please enter an even number");
-    while (!(players === 0 || !!(players && !(players%2)))) {
-      players = prompt("You need to enter an even number. How many players?");
-    }
-    console.log(players);
-    var playerArray = new Array(parseInt(players));
-    console.log(playerArray);
-    for (var i = 0; i < parseInt(players); i++) {
-      playerArray[i] = i + 1;
-    }
-    function random_item(items) {
-      return items[Math.floor(Math.random()*items.length)];
-    }
-    var player1 = random_item(playerArray);
-    do {
-      var player2 = random_item(playerArray);
-    } while (player1 == player2);
-
-    playerArray[player1-1] = null;
-    playerArray[player2-1] = null;
-
-    var round1Winners = new Array(playerArray.length/2);
-    var count = 0;
-
-    alert("First match is Player " + player1 + " vs Player " + player2);
-
-    if (progRunning) {
-      // Stop the ball
-      progRunning = false;
-      clearInterval(interval);
-    } else {
-      // Start the ball
-      progRunning = true;
-      interval = setInterval(function(){
-
-        ball.css({
-          "left": posX + "px",
-          "top": posY + "px"
-        })
-
-        var ballLeft = ball.offset().left;
-        var ballRight = ballLeft + ball.width();
-        var ballTop = ball.offset().top;
-        var ballBottom = ballTop + ball.height();
-
-        var containerLeft = container.offset().left;
-        var containerRight = container.offset().left + container.width();
-        var containerTop = container.offset().top + p;
-        var containerBottom = containerTop + container.height();
-
-        var paddleLeft = paddle.offset().left;
-        var paddleRight = paddleLeft + paddle.width();
-        var paddleTop = paddle.offset().top;
-        var paddleBottom = paddleTop + paddle.height();
-
-        var paddle2Left = paddle2.offset().left;
-        var paddle2Right = paddle2Left + paddle2.width();
-        var paddle2Top = paddle2.offset().top;
-        var paddle2Bottom = paddle2Top + paddle2.height();
-
-        paddle.css({
-          "top": padposy
-        })
-
-        paddle2.css({
-          "top": pad2posy
-        })
-
-        if (up) {
-          if (padposy >= 3) {
-            padposy -= 3;
-          }
-          //console.log("going up");
-        }
-
-        if (down) {
-          if (padposy <= 239) {
-            padposy += 3;
-          }
-          //console.log("going down");
-        }
-
-        if (up2) {
-          if (pad2posy >= 3) {
-            pad2posy -= 3;
-          }
-          //console.log("2 going up");
-        }
-
-        if (down2) {
-          if (pad2posy <= 239) {
-            pad2posy += 3;
-          }
-          //console.log("2 going down");
-        }
-
-        // Ball movement
-        if (directionX === "+") {
-          posX+=4;
-        } else if (directionX === "-") {
-          posX-=4;
-        }
-
-        if (directionY === "+") {
-          gravityspeed += gravity;
-          posY+= gravityspeed;
-        } else if (directionY === "-"){
-          gravityspeed += gravity;
-          posY+= gravityspeed;
-        }
-
-        if (ballRight >= containerRight) {
-          directionX = "-";
-        } else if (ballLeft <= containerLeft) {
-          directionX = "+";
-        }
-
-        if (ballBottom >= containerBottom) {
-          directionY = "-";
-          gravityspeed = -5;
-        } else if (ballTop <= containerTop) {
-          directionY = "+";
-        }
-
-        if (ballLeft <= paddleRight && ballTop < paddleBottom && ballBottom > paddleTop) {
-          directionX = "+";
-          //console.log("Paddle contact");
-        }
-
-        if (ballTop == paddle2Top) {
-          //console.log("equal");
-        }
-
-        if (ballRight >= paddle2Left && ballTop < paddle2Bottom && ballBottom > paddle2Top) {
-          // lastMoveHit = true;
-          directionX = "-";
-          //console.log("Paddle 2 contact");
-        }
-
-        if (ballRight > containerRight) {
-          console.log("Hello");
-          score1++;
-          $("#scr1").html("Score 1: " + score1);
-          posX = 550;
-          posY = 0;
-          directionX = "-";
-          directionY = "+";
-          gravity = 0.05;
-          gravityspeed = 0;
-        }
-
-        if (ballLeft < containerLeft) {
-          score2++;
-          $("#scr2").html("Score 2: " + score2);
-          posX = 25;
-          posY = 0;
-          directionX = "+";
-          directionY = "+";
-          gravity = 0.05;
-          gravityspeed = 0;
-        }
-
-        if(score1 == 1) {
-          console.log("Player " + player1 + " has won!");
-          winner = player1;
-          // alert("Player 1 has won!");
-          //prompt("Please enter your name", "Harry Potter");
-        } else if (score2 == 1) {
-          console.log("Player " + player2 + " has won!");
-          winner = player2;
-          // alert("Player2 has won!")
-          //prompt("Please enter your name", "Harry Potter");
-        }
-
-        if(winner != null) {
-
-          alert("Winner is Player " + winner);
-          round1Winners[count] = winner;
-          count++;
-          console.log(playerArray);
-          console.log(round1Winners);
-          score1 = 0;
-          score2 = 0;
-          $("#scr1").html("Score 1: " + score1);
-          $("#scr2").html("Score 2: " + score2);
-          if (roundCount > 1) {
-            filtered = filtered.filter(function (el) {
-              return el != null;
-            });
-          } else {
-            filtered = playerArray.filter(function (el) {
-              return el != null;
-            });
-          }
-          console.log("Filtered is " + filtered);
-          player1 = random_item(filtered);
-          filtered[filtered.indexOf(player1)] = null;
-          filtered = filtered.filter(function (el) {
-            return el != null;
-          });
-          player2 = random_item(filtered);
-          console.log(filtered);
-          console.log(playerArray);
-          winner = null;
-          if (playerArray.allValuesSame()){
-            alert("Round " + roundCount + " Finished!");
-            alert("Winners of Round " + roundCount + " are: " + round1Winners);
-            count = 0;
-            roundCount++;
-            playerArray = round1Winners;
-            player1 = random_item(playerArray);
-            playerArray[playerArray.indexOf(player1)] = null;
-            filtered = playerArray.filter(function (el) {
-              return el != null;
-            });
-            player2 = random_item(filtered);
-            playerArray[playerArray.indexOf(player2)] = null;
-            filtered = playerArray.filter(function (el) {
-              return el != null;
-            });
-            roundBegin = true;
-
-          }
-          // for (var i = 0; i < playerArray.length; i++) {
-          //   if (playerArray[i] != null) {
-          //     round1Finished = false;
-          //   }
-          // }
-          if (roundBegin == false) {
-            playerArray[playerArray.indexOf(player1)] = null;
-            playerArray[playerArray.indexOf(player2)] = null;
-          }
-          roundBegin = false;
-          alert("Starting next game: Player " + player1 + " vs " + player2);
-        }
-
-        // console.log(i + " seconds");
-        // i++;
-      }, 10);
-    }
-  })
 
   var up = false;
   var down = false;
@@ -387,6 +92,105 @@ $(function(){
     }
   });
 
+  $("#btn").click(function(){
+    while(paddle1Color != "1" && paddle1Color != "2" && paddle1Color != "3" && paddle1Color != "4" && paddle1Color != "5") {
+      var paddle1Color = prompt("Player 1, choose your paddle colour: 1) Red, 2) Yellow, 3) Blue, 4) Orange, 5) Purple");
+    }
+    while(paddle2Color != "1" && paddle2Color != "2" && paddle2Color != "3" &&    paddle2Color != "4" && paddle2Color != "5") {
+      var paddle2Color = prompt("Player 2, choose your paddle colour: 1) Red, 2) Yellow, 3) Blue, 4) Orange, 5) Purple");
+    }
+    switch (paddle1Color) {
+      case "1": paddle1Color = "Red"; break;
+      case "2": paddle1Color = "Yellow"; break;
+      case "3": paddle1Color = "Blue"; break;
+      case "4": paddle1Color = "Orange"; break;
+      case "5": paddle1Color = "Purple"; break;
+      default: break;
+    }
+    switch (paddle2Color) {
+      case "1": paddle2Color = "Red"; break;
+      case "2": paddle2Color = "Yellow"; break;
+      case "3": paddle2Color = "Blue"; break;
+      case "4": paddle2Color = "Orange"; break;
+      case "5": paddle2Color = "Purple"; break;
+      default: break;
+    }
+    paddle.css("backgroundColor", paddle1Color);
+    paddle2.css("backgroundColor", paddle2Color);
+    playGame("1", "2");
+    counter = 0;
+  })
+
+  $("#tournament").click(function(){
+    tournament = true;
+    var players = prompt("How many players? Please enter an even number");
+    while (!(players === 0 || (players && !(players%2)))) {
+      players = prompt("You need to enter an even number. How many players?");
+    }
+    playerArray = new Array(parseInt(players));
+
+    for (var i = 0; i < parseInt(players); i++) {
+      playerArray[i] = i + 1;
+    }
+
+    var player1 = random_player(playerArray);
+    do {
+      var player2 = random_player(playerArray);
+    } while (player1 == player2);
+
+    round1Winners = new Array(playerArray.length/2);
+
+    playerArray[player1-1] = null;
+    playerArray[player2-1] = null;
+
+    var count = 0;
+
+    alert("First match is Player " + player1 + " vs Player " + player2);
+
+    var win = playGame(player1, player2);
+
+    if (roundCount > 1) {
+      filtered = filtered.filter(function (el) {
+        return el != null;
+      });
+    } else {
+      filtered = playerArray.filter(function (el) {
+        return el != null;
+      });
+    }
+
+    player1 = random_player(filtered);
+    filtered[filtered.indexOf(player1)] = null;
+    filtered = filtered.filter(function (el) {
+      return el != null;
+    });
+    player2 = random_player(filtered);
+    winner = null;
+
+    if (playerArray.allValuesSame()){
+      alert("Round " + roundCount + " Finished!");
+      alert("Winners of Round " + roundCount + " are: " + round1Winners);
+      count = 0;
+      roundCount++;
+      playerArray = round1Winners;
+      player1 = random_player(playerArray);
+      playerArray[playerArray.indexOf(player1)] = null;
+      filtered = playerArray.filter(function (el) {
+        return el != null;
+      });
+      player2 = random_player(filtered);
+      playerArray[playerArray.indexOf(player2)] = null;
+      filtered = playerArray.filter(function (el) {
+        return el != null;
+      });
+      roundBegin = true;
+    }
+  })
+
+  function random_player(players) {
+    return players[Math.floor(Math.random()*players.length)];
+  }
+
   function playGame(player1, player2) {
     if (progRunning) {
       // Stop the ball
@@ -394,7 +198,7 @@ $(function(){
       clearInterval(interval);
     } else {
       // Start the ball
-      progRunning = true;
+      // progRunning = true;
       interval = setInterval(function(){
 
         ball.css({
@@ -434,28 +238,24 @@ $(function(){
           if (padposy >= 3) {
             padposy -= 3;
           }
-          console.log("going up");
         }
 
         if (down) {
           if (padposy <= 239) {
             padposy += 3;
           }
-          console.log("going down");
         }
 
         if (up2) {
           if (pad2posy >= 3) {
             pad2posy -= 3;
           }
-          console.log("2 going up");
         }
 
         if (down2) {
           if (pad2posy <= 239) {
             pad2posy += 3;
           }
-          console.log("2 going down");
         }
 
         // Ball movement
@@ -488,22 +288,17 @@ $(function(){
 
         if (ballLeft <= paddleRight && ballTop < paddleBottom && ballBottom > paddleTop) {
           directionX = "+";
-          console.log("Paddle contact");
         }
 
         if (ballTop == paddle2Top) {
-          console.log("equal");
         }
 
         if (ballRight >= paddle2Left && ballTop < paddle2Bottom && ballBottom > paddle2Top) {
-          // lastMoveHit = true;
           directionX = "-";
-          console.log("Paddle 2 contact");
         }
 
         if (ballRight > containerRight) {
           score1++;
-          console.log("Hello");
           $("#scr1").html("Score 1: " + score1);
           posX = 550;
           posY = 0;
@@ -524,25 +319,82 @@ $(function(){
           gravityspeed = 0;
         }
 
-        if(score1 == 1) {
-          winner = player1;
-          alert("The winner is Player 1");
-          clearInterval(interval);
-          //alert("Player 1 has won!");
-          //prompt("Please enter your name", "Harry Potter");
-        } else if (score2 == 1) {
-          winner = player2;
-          alert("The winner is Player 2");
-          clearInterval(interval);
-          //alert("Player2 has won!")
-          //prompt("Please enter your name", "Harry Potter");
-        }
+        if(score1 == 1 || score2 == 1) {
+          if (score1 == 1) {
+            winner = player1;
+          } else {
+            winner = player2;
+          }
+          score1 = 0;
+          score2 = 0;
+          $("#scr1").html("Score 1: " + score1);
+          $("#scr2").html("Score 2: " + score2);
+          alert("The winner is Player " + winner);
+          if (tournament == true && winner != null) {
+            round1Winners[counter] = winner;
+            counter++;
+            playerArray = playerArray.filter(function (el) {
+              return el != null;
+            });
+            player1 = random_player(playerArray);
+            playerArray[playerArray.indexOf(player1)] = null;
+            playerArray = playerArray.filter(function (el) {
+              return el != null;
+            });
+            player2 = random_player(playerArray);
+            winner = null;
 
-        // console.log(i + " seconds");
-        // i++;
+            if (playerArray.length == 0){
+              if (roundCount > 1) {
+                round1Winners.length = round1Winners.length / 2;
+              }
+              alert("Round " + roundCount + " Finished!");
+              if (round1Winners.length == 1) {
+                alert("Player " + round1Winners[0] + " wins the tournament!");
+                posX = 25;
+                posY = 0;
+                round1Winners.shift();
+                playerArray.shift();
+                ball.css({
+                  "left": posX + "px",
+                  "top": posY + "px"
+                })
+                tournament = false;
+                roundCount = 1;
+                clearInterval(interval);
+                t1finished = true;
+                counter = 0;
+                return;
+              }
+              alert("Winners of Round " + roundCount + " are: " + round1Winners);
+              counter = 0;
+              roundCount++;
+              playerArray = round1Winners;
+              player1 = random_player(playerArray);
+              playerArray[playerArray.indexOf(player1)] = null;
+              playerArray = playerArray.filter(function (el) {
+                return el != null;
+              });
+              player2 = random_player(playerArray);
+              roundBegin = true;
+            }
+
+            playerArray[playerArray.indexOf(player2)] = null;
+
+            alert("Next match is Player " + player1 + " vs Player " + player2)
+          } else {
+            posX = 25;
+            posY = 0;
+            ball.css({
+              "left": posX + "px",
+              "top": posY + "px"
+            })
+            clearInterval(interval);
+          }
+        }
       }, 10);
+      return winner;
     }
   }
-
 
 })
